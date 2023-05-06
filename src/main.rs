5c49b1c0b9ec3526f847770c0e8f1affed576130
MarkGhebrial <mghebrialjr@icloud.com>
2023-05-06 15:28:35 -0700

    Parse packets and their payloads

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use socket2::{Domain, Protocol, Socket, Type};
 use std::io::Read;
+use std::io::Write;
+
+use etherparse::{Icmpv4Slice, Icmpv4Type};
+use etherparse::{Ipv4HeaderSlice, SlicedPacket};
 
 fn open_socket() -> Socket {
     // Create the ICMP socket
@@ -14,8 +18,26 @@ fn main() {
     let mut buf = [0u8; 1024];
 
     loop {
+        // Get a packet
         let packet_len = socket.read(&mut buf).unwrap();
+        let packet = &buf[..packet_len];
 
         println!("RECIEVED {:?}", &buf[..packet_len]);
+
+        // Parse the IP header
+        let ipheader = Ipv4HeaderSlice::from_slice(packet).unwrap();
+
+        // Parse the icmp header
+        let icmp = Icmpv4Slice::from_slice(
+            &packet[(ipheader.total_len() - ipheader.payload_len()) as usize..],
+        )
+        .unwrap();
+
+        match icmp.icmp_type() {
+            Icmpv4Type::EchoRequest(header) => {
+                println!("THAT'S AN ECHO REQUEST!!! {:?}", header);
+            }
+            t => println!("{:?}", t),
+        }
     }
 }
