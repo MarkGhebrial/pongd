7843876cb9c65937948e9a913ba6f47ddab83cd1
MarkGhebrial <mghebrialjr@icloud.com>
2023-05-06 22:38:24 -0700

    IT WORKS!!!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use socket2::{Domain, Protocol, Socket, Type, SockAddr};
-use std::net::{SocketAddrV4, Ipv4Addr};
+use socket2::{Domain, Protocol, SockAddr, Socket, Type};
+use std::io::Cursor;
 use std::io::Read;
 use std::io::Write;
-use std::io::Cursor;
+use std::net::{Ipv4Addr, SocketAddrV4};
 
 use std::fs::File;
 
-use etherparse::{Icmpv4Slice, Icmpv4Type};
-use etherparse::{Ipv4HeaderSlice, PacketBuilder};
+use etherparse::Ipv4HeaderSlice;
+use etherparse::{IcmpEchoHeader, Icmpv4Header, Icmpv4Slice, Icmpv4Type, Icmpv6Type, Ipv4Header, IpNumber};
 
 fn open_socket() -> Socket {
     // Create the ICMP socket
@@ -29,11 +29,11 @@ fn main() {
         println!("RECIEVED {:?}", &buf[..packet_len]);
 
         // Parse the IP header
-        let ipheader = Ipv4HeaderSlice::from_slice(packet).unwrap();
+        let incoming_ip_header = Ipv4HeaderSlice::from_slice(packet).unwrap();
 
         // Parse the icmp header
         let icmp = Icmpv4Slice::from_slice(
-            &packet[(ipheader.total_len() - ipheader.payload_len()) as usize..],
+            &packet[(incoming_ip_header.total_len() - incoming_ip_header.payload_len()) as usize..],
         )
         .unwrap();
 
@@ -41,30 +41,57 @@ fn main() {
             Icmpv4Type::EchoRequest(header) => {
                 println!("THAT'S AN ECHO REQUEST!!! {:?}", header);
 
-                //dbg!(ipheader.destination(), ipheader.source());
-                dbg!(header.id, header.seq);
+                let data_len = icmp.payload().len() - icmp.header_len();
+                println!("INCOMING PAYLOAD LEN {}", icmp.payload().len() - icmp.header_len());
+                //println!("PAYLOAD {:?}", &icmp.payload());
 
-                let builder =
-                    PacketBuilder::ethernet2([0; 6], [0; 6])
-                        .ipv4(ipheader.destination(), ipheader.source(), ipheader.ttl())
-                        .icmpv4_echo_reply(header.id, header.seq);
+                let mut icmp_header = Icmpv4Header::new(Icmpv4Type::EchoReply(IcmpEchoHeader {
+                    id: header.id,
+                    seq: header.seq,
+                }));
 
-                //dbg!(icmp.payload().iter().map(|c| *c as char).collect::<String>());
+                icmp_header.update_checksum(&icmp.payload());
 
-                dbg!(&icmp.payload()[8..]);
+                // let dest_addr =
+                //     SockAddr::from(SocketAddrV4::new(Ipv4Addr::from(incoming_ip_header.source()), 0));
 
-                //let mut buffer = File::create("test.txt").unwrap();
-                let buffer = vec![];
+                let ip_header = Ipv4Header::new(
+                    (data_len + icmp_header.header_len()) as u16,
+                    incoming_ip_header.ttl(),
+                    IpNumber::Icmp as u8,
+                    incoming_ip_header.destination(),
+                    incoming_ip_header.source()
+                );
+                
+                let buffer: Vec<u8> = vec![];
                 let mut c = Cursor::new(buffer);
-                builder.write(&mut c, &icmp.payload()[16..]).unwrap();
 
-                println!("Outbound packet: {:?}", c);
+                // Write the ip and icmp headers to the buffer
+                //ip_header.write(&mut c).unwrap();
+                //c.set_position(ip_header.header_len() as u64);
+
+                icmp_header.write(&mut c).unwrap();
+
+                // Write the icmp payload to the buffer
+                //c.write(&icmp.bytes5to8()).unwrap();
+                c.write(&icmp.payload()).unwrap();
+
+                //println!("{:?}", c);
+                //println!("ICMP Payload: {:?}", &icmp.payload());
+
+                //builder.write(&mut c, &icmp.payload()[16..]).unwrap();
 
                 let outbound_packet: &[u8] = c.get_ref().as_slice();
 
-                let dest_addr = SockAddr::from(SocketAddrV4::new(Ipv4Addr::from(ipheader.source()), 0));
+                println!("SENDING {:?}", outbound_packet);
+
+                let dest_addr =
+                    SockAddr::from(SocketAddrV4::new(Ipv4Addr::from(incoming_ip_header.source()), 0));
 
                 socket.send_to(outbound_packet, &dest_addr).unwrap();
+                socket.flush().unwrap();
+
+
 
                 //let outbound_packet: &[u8] = builder.into();
 
